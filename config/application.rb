ad299ff67aa24362af552d6b617a79b6cf1276c0
Nardo Nykolyszyn <nardonykolyszyn@gmail.com>
2019-07-28 17:40:34 +0200

    remove useless rails core dependencies

--- config/application.rb
+++ config/application.rb
@@ -6,9 +6,6 @@ require "active_model/railtie"
 require "active_job/railtie"
 require "active_record/railtie"
 require "action_controller/railtie"
-require "action_mailer/railtie"
-require "action_view/railtie"
-require "action_cable/engine"
 require "sprockets/railtie"
 
 # Require the gems listed in Gemfile, including any gems
