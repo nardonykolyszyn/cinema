c465ca92f51019afbd55dfb88183b5842bdda24e
Nardo Nykolyszyn <nardonykolyszyn@gmail.com>
2019-07-28 20:04:54 +0200

    add model specs

--- spec/models/room_spec.rb
+++ spec/models/room_spec.rb
@@ -7,7 +7,19 @@ RSpec.describe Room, type: :model do
     @room = create(:room)
   end
 
-  it 'is valid with valid attrs' do
-    expect(@room).to be_valid
+  describe 'saving valid object' do
+    it 'is valid with valid attrs' do
+      expect(@room).to be_valid
+    end
+  end
+
+  describe 'validations' do
+    it { should validate_presence_of(:name) }
+    it { should validate_uniqueness_of(:name) }
+    it { should validate_presence_of(:capacity) }
+  end
+
+  describe 'aasm transitions' do
+    it { should have_states :available, :busy, :unavailable }
   end
 end
