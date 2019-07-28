b4934aeb67756e08220a3ab7beb9cb87f557935f
Nardo Nykolyszyn <nardonykolyszyn@gmail.com>
2019-07-28 19:36:11 +0200

    remove transactional fixtures

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -47,6 +47,7 @@ RSpec.configure do |config|
   # The different available types are documented in the features, such as in
   # https://relishapp.com/rspec/rspec-rails/docs
   config.infer_spec_type_from_file_location!
+  config.use_transactional_fixtures = false
 
   # Filter lines from Rails gems in backtraces.
   config.filter_rails_from_backtrace!
