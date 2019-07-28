79527f2f7775634693b21ff3e45b1ef2a5531f84
Nardo Nykolyszyn <nardonykolyszyn@gmail.com>
2019-07-29 00:42:58 +0200

    add factory for show_function

---
+++ spec/factories/show_functions.rb
@@ -0,0 +1,8 @@
+FactoryBot.define do
+  factory :show_function do
+    function { nil }
+    client { nil }
+    starts_at { "2019-07-29 00:41:52" }
+    ends_at { "2019-07-29 00:41:52" }
+  end
+end
