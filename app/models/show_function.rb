c6a6fd3663320645557c87ede5c97d8d5d53cc0c
Nardo Nykolyszyn <nardonykolyszyn@gmail.com>
2019-07-29 21:30:30 +0200

    block room

--- app/models/show_function.rb
+++ app/models/show_function.rb
@@ -14,6 +14,8 @@
 #
 
 class ShowFunction < ApplicationRecord
+  ## Callbacks
+  after_save :block_room
   ## Validations
   validate :available_spots
   ## Associations
@@ -22,10 +24,16 @@ class ShowFunction < ApplicationRecord
 
   private
 
+  def block_room
+    function.room.busy! if limit_reached?
+  end
+
+  def limit_reached?
+    function.current_show_functions.size >= function.room_capacity
+  end
+
   # It validates that show_functions are not higher than room capacity.
   def available_spots
-    if function.current_show_functions.size >= function.room_capacity
-      errors.add(:function, 'room has reached its maximum capacity')
-    end
+    errors.add(:function, 'room has reached its maximum capacity') if limit_reached?
   end
 end
