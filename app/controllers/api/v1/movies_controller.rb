19e3f5bdba16f3c3ed7f73264b596ce0968a7014
Nardo Nykolyszyn <nardonykolyszyn@gmail.com>
2019-07-29 03:06:10 +0200

    add movies controller

--- app/controllers/api/v1/movies_controller.rb
+++ app/controllers/api/v1/movies_controller.rb
@@ -3,6 +3,39 @@
 module Api
   module V1
     class MoviesController < ApiSitesController
+      def index
+        @movies = Movie.all
+        render json: { movies: MovieSerializer.new(@movies).serializable_hash }
+      end
+
+      def show
+        @movie = Movie.find(params[:id])
+        render json: { movie: MovieSerializer.new(@movie) }
+      end
+
+      def create
+        @movie = Movie.new(movie_params)
+        if @movie.save
+          @response = { movie: MovieSerializer.new(@movie) }
+        else
+          @response = { errors: @movie.errors }
+        end
+        render json: @response
+      end
+
+      def update
+      end
+
+      def destroy
+      end
+
+      private
+
+      def movie_params
+        params.require(:movie).permit(
+          :name, :year, :gender
+        )
+      end
     end
   end
 end
