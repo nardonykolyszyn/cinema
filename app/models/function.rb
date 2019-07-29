ba56cf95c721b4839b3b30047efc3affc882e9ee
Nardo Nykolyszyn <nardonykolyszyn@gmail.com>
2019-07-29 17:36:58 +0200

    add function model

--- app/models/function.rb
+++ app/models/function.rb
@@ -1,5 +1,18 @@
 # frozen_string_literal: true
 
+# == Schema Information
+#
+# Table name: functions
+#
+#  id         :bigint           not null, primary key
+#  show_at    :date
+#  movie_id   :bigint
+#  room_id    :bigint
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
+
 class Function < ApplicationRecord
   ## Validations
   validates :show_at, presence: true
