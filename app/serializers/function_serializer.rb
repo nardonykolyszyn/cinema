0decd96d551c34fc3020bea9aed0e02159a03458
Nardo Nykolyszyn <nardonykolyszyn@gmail.com>
2019-07-29 20:31:41 +0200

    add functions serializer

---
+++ app/serializers/function_serializer.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+# == Schema Information
+#
+# Table name: functions
+#
+#  id         :bigint           not null, primary key
+#  show_at    :date
+#  movie_id   :bigint
+#  room_id    :bigint
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
+class FunctionSerializer < ActiveModel::Serializer
+  attributes :id, :show_at, :room, :movie
+
+  def room
+    RoomSerializer.new(object.room).attributes
+  end
+
+  def movie
+    MovieSerializer.new(object.movie).attributes
+  end
+end
