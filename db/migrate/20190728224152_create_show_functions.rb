4834414d58cd49ea32d48f77a6fa75f2b9a2f140
Nardo Nykolyszyn <nardonykolyszyn@gmail.com>
2019-07-29 17:46:06 +0200

    remove client reference

--- db/migrate/20190728224152_create_show_functions.rb
+++ db/migrate/20190728224152_create_show_functions.rb
@@ -3,8 +3,7 @@
 class CreateShowFunctions < ActiveRecord::Migration[5.2]
   def change
     create_table :show_functions do |t|
-      t.belongs_to :function, foreign_key: { on_delete: :cascade }
-      t.belongs_to :client, foreign_key: { on_delete: :cascade }
+      t.belongs_to :function, foreign_key: { on_delete: :cascade }, index: true
       t.datetime :starts_at
       t.datetime :ends_at
 
