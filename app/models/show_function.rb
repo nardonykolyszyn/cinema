f9c6799e0ba5afac02400724d56a0ba190ab9c14
Nardo Nykolyszyn <nardonykolyszyn@gmail.com>
2019-07-29 17:45:46 +0200

    add show_function model

--- app/models/show_function.rb
+++ app/models/show_function.rb
@@ -1,3 +1,16 @@
+# == Schema Information
+#
+# Table name: show_functions
+#
+#  id          :bigint           not null, primary key
+#  function_id :bigint
+#  starts_at   :datetime
+#  ends_at     :datetime
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#  client_id   :bigint
+#
+
 class ShowFunction < ApplicationRecord
   belongs_to :function
   belongs_to :client
