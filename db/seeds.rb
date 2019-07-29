453cb478f36e7627541c6d69a0406598fd16f488
Nardo Nykolyszyn <nardonykolyszyn@gmail.com>
2019-07-29 17:46:14 +0200

    add movies to seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,12 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# frozen_string_literal: true
+
+# Create movies
+MOVIE_GENDERS = %i[horror romantic comedy adventure action].freeze
+
+20.times do
+  Movie.create(
+    name: Faker::FunnyName.unique.name,
+    gender: MOVIE_GENDERS.sample,
+    year: rand((1990..Time.now.year))
+  )
+end
