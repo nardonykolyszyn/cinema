c088b66a0e9e13cbc8cda41eaf0d8b04986fd4b6
Nardo Nykolyszyn <nardonykolyszyn@gmail.com>
2019-07-29 17:45:54 +0200

    add index

--- db/migrate/20190728223244_create_functions.rb
+++ db/migrate/20190728223244_create_functions.rb
@@ -4,8 +4,8 @@ class CreateFunctions < ActiveRecord::Migration[5.2]
   def change
     create_table :functions do |t|
       t.date :show_at
-      t.belongs_to :movie, foreign_key: { on_delete: :cascade }
-      t.belongs_to :room, foreign_key: { on_delete: :cascade }
+      t.belongs_to :movie, foreign_key: { on_delete: :cascade }, index: true
+      t.belongs_to :room, foreign_key: { on_delete: :cascade }, index: true
 
       t.timestamps
     end
