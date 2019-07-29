5742c04389d84d01411d1ffcfb1d305d9c5388d9
Nardo Nykolyszyn <nardonykolyszyn@gmail.com>
2019-07-29 19:55:05 +0200

    add seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,3 +10,27 @@ MOVIE_GENDERS = %i[horror romantic comedy adventure action].freeze
     year: rand((1990..Time.now.year))
   )
 end
+
+# Create client
+5.times do
+  Client.create(
+    first_name: Faker::Name.first_name,
+    last_name: Faker::Name.last_name,
+    document_type: 'ce',
+    document_number: '12345678'
+  )
+end
+
+# Create rooms
+%w[vip super luxury].each { |n| Room.new(capacity: 10, name: n).save }
+
+# Create functions
+Room.all.each do |room|
+  5.times do
+    Function.create(
+      show_at: Time.now + 1.day,
+      movie: Movie.all.sample,
+      room: Room.all.sample
+    )
+  end
+end
