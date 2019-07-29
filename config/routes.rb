14d931e75a0e92294eff498b5a7f47bc29c47b36
Nardo Nykolyszyn <nardonykolyszyn@gmail.com>
2019-07-29 20:05:01 +0200

    add resources

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,10 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :movies
+      resources :rooms
+      resources :functions do
+        resources :show_functions, only: %i[index create destroy]
+      end
     end
   end
 end
