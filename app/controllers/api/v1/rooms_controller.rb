5e005e240b0631b4c4f77e55f31a3968aded91fe
Nardo Nykolyszyn <nardonykolyszyn@gmail.com>
2019-07-29 20:24:03 +0200

    add rooms controller

---
+++ app/controllers/api/v1/rooms_controller.rb
@@ -0,0 +1,69 @@
+# frozen_string_literal: true
+
+module Api
+  module V1
+    class RoomsController < ApiSitesController
+      skip_before_action :assign_resource, only: %i[index create]
+
+      def index
+        @rooms = Room.all.paginate(page: @page, per_page: 10)
+        render json: {
+          rooms: ActiveModelSerializers::SerializableResource.new(@rooms),
+          status: :ok
+        }
+      end
+
+      def show
+        render json: {
+          room: RoomSerializer.new(@room),
+          status: :ok
+        }
+      end
+
+      def create
+        @room = Room.new(room_params)
+        @response = if @room.save
+                      { room: RoomSerializer.new(@room) }
+                    else
+                      {
+                        errors: @room.errors.full_messages.to_sentence,
+                        status: :unprocessable_entity
+                      }
+                    end
+        render json: @response
+      end
+
+      def update
+        @response = if @room.update(room_params)
+                      { room: RoomSerializer(@room), status: :ok }
+                    else
+                      {
+                        errors: @room.errors.full_messages.to_sentence,
+                        status: :unprocessable_entity
+                      }
+                    end
+        render json: @response
+      end
+
+      def destroy
+        @response = if @room.destroy
+                      { message: 'Room has been deleted successfully' }
+                    else
+                      {
+                        errors: @room.errors.full_messages.to_sentence,
+                        status: :unprocessable_entity
+                      }
+                    end
+        render json: @response
+      end
+
+      private
+
+      def room_params
+        params.require(:room).permit(
+          :name, :capacity
+        )
+      end
+    end
+  end
+end
