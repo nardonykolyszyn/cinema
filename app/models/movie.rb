851557cfebb116aeb7e7f8600d2e28f0bbc702e0
Nardo Nykolyszyn <nardonykolyszyn@gmail.com>
2019-07-29 17:37:06 +0200

    annotate on movies model

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,8 +1,21 @@
 # frozen_string_literal: true
 
+# == Schema Information
+#
+# Table name: movies
+#
+#  id         :bigint           not null, primary key
+#  name       :string
+#  year       :integer
+#  gender     :integer          default("horror")
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 class Movie < ApplicationRecord
   ## Enum method
-  enum gender: %i[horror romantic comedy adventure].freeze
+  enum gender: %i[horror romantic comedy adventure action].freeze
   ## Associations
+  has_many :functions
   ## Validations
 end
