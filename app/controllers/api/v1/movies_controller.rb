9071ad5512616bbb63977e5e708a6237a0f4d3ca
Nardo Nykolyszyn <nardonykolyszyn@gmail.com>
2019-07-29 21:09:31 +0200

    remove callback override

--- app/controllers/api/v1/movies_controller.rb
+++ app/controllers/api/v1/movies_controller.rb
@@ -3,8 +3,7 @@
 module Api
   module V1
     class MoviesController < ApiSitesController
-      skip_before_action :assign_resource, only: %i[index create]
-
+      
       def index
         @movies = Movie.all.paginate(page: @page, per_page: 10)
         render json: {
