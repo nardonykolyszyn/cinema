ff5e34e97d9b8afce64ac12d32e69faa8db39428
Nardo Nykolyszyn <nardonykolyszyn@gmail.com>
2019-07-29 03:06:32 +0200

    add validations comments

--- app/models/function.rb
+++ app/models/function.rb
@@ -4,6 +4,8 @@ class Function < ApplicationRecord
   ## Validations
   validates :show_at, presence: true
   ## Associations
+  has_many :function_movies
+  has_many :movies, through: :function_movies
   belongs_to :movie
   belongs_to :room
 end
