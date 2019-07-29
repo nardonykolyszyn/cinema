b207e6d56d6695415465439075c97594d76bbac0
Nardo Nykolyszyn <nardonykolyszyn@gmail.com>
2019-07-29 17:36:43 +0200

    add before_action callbacks

--- app/controllers/api/v1/api_sites_controller.rb
+++ app/controllers/api/v1/api_sites_controller.rb
@@ -4,6 +4,22 @@ module Api
   module V1
     class ApiSitesController < ApplicationController
       protect_from_forgery with: :null_session
+      before_action :assign_resource
+      before_action :assign_page
+
+      private
+
+      # It assigns a default param for pagination.
+      def assign_page
+        @page = params[:page] || 1
+      end
+
+      # Automatically find the model by id.
+      def assign_resource
+        resource_name = controller_name.singularize
+        resource = controller_name.classify.constantize.find(params[:id])
+        instance_variable_set("@#{resource_name}", resource)
+      end
     end
   end
 end
