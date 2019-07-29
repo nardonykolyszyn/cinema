65d2b705ad1a826dddc47dbd2afc555753b36303
Nardo Nykolyszyn <nardonykolyszyn@gmail.com>
2019-07-29 18:31:27 +0200

    add magic comment

--- app/models/show_function.rb
+++ app/models/show_function.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # == Schema Information
 #
 # Table name: show_functions
@@ -12,6 +14,18 @@
 #
 
 class ShowFunction < ApplicationRecord
+  ## Validations
+  validate :available_spots
+  ## Associations
   belongs_to :function
   belongs_to :client
+
+  private
+
+  # It validates that show_functions are not higher than room capacity.
+  def available_spots
+    if function.current_show_functions.size >= function.room_capacity
+      errors.add(:function, 'room has reached its maximum capacity')
+    end
+  end
 end
